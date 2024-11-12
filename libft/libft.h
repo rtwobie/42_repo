2508a2d6870216eabba15288ab740095b72a9147
R2B <robeats.hale@gmail.com>
2024-11-12 19:55:29 +0100

    add: reflects all working function in file

--- libft/libft.h
+++ libft/libft.h
@@ -6,21 +6,30 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 20:18:14 by rha-le            #+#    #+#             */
-/*   Updated: 2024/10/15 20:20:23 by rha-le           ###   ########.fr       */
+/*   Updated: 2024/11/12 17:21:40 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	isalnum(int c);
-int	ft_atoi(const char *str);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_strlen(const char *s);
-int	toupper(int c);
-int	toupper(int c);
+# include <stdlib.h>
+
+int		ft_isalnum(int c);
+int		ft_atoi(const char *nptr);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_isspace(int c);
+int		ft_strlen(const char *s);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 #endif
