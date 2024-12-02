5a0c5a7d1de5c79c402ef99dd2c1a10b5846789d
R2B <robeats.hale@gmail.com>
2024-12-02 22:58:27 +0100

    feat: new function

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 23:06:01 by rha-le            #+#    #+#             */
-/*   Updated: 2024/12/01 00:38:35 by rha-le           ###   ########.fr       */
+/*   Updated: 2024/12/02 21:27:00 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 
 char	*ft_substr(const char *str, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	j;
-	char	*substr;
+	size_t			i;
+	size_t			j;
+	char			*substr;
 
+	if (start >= (unsigned int)ft_strlen(str))
+		return (ft_strdup(""));
+	if (len > (unsigned int)ft_strlen(str + start))
+		len = (unsigned int)ft_strlen(str + start);
 	substr = (char *)malloc(sizeof(*str) * (len + 1));
 	if (!substr)
 		return (NULL);
@@ -40,7 +44,16 @@ char	*ft_substr(const char *str, unsigned int start, size_t len)
 /*#include <stdio.h>*/
 /*int main(void)*/
 /*{*/
-/*	char *sub = ft_substr("Hallo", 8, 13);*/
+/*	char *sub = ft_substr("Bonjour comment ca va?", 5, 8);*/
 /*	printf("%s\n", sub);*/
 /*	free(sub);*/
 /*}*/
+
+/* QUESTION:
+ *		ln. 23: Why empty string and not NULL?
+ */
+
+/* NOTE:
+ *		ln. 24: handles exceeding len value
+ *				if bigger than ft_strlen(str + start).
+ */
