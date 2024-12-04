9f735d164c098fce524341508e4a98bb2ee1c3b0
Robbie Nhat Anh Ha Le <rha-le@c3a2c3.42berlin.de>
2024-12-04 17:27:46 +0100

    chore: itoa

--- libft/libft.h
+++ libft/libft.h
@@ -43,6 +43,7 @@ char	*ft_substr(const char *str, unsigned int start, size_t len);
 char	*ft_strjoin(const char *str1, const char *str2);
 char	*ft_strtrim(const char *s, const char *set);
 char	**ft_split(const char *s, char c);
+char	*ft_itoa(int n);
 
 // TODO:
 //		[ ] clean up Makefile
