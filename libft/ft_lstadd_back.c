ddb96692fd274633881fddc7c5d258088c42f2ad
Robbie Nhat Anh Ha Le <rha-le@c3a10c6.42berlin.de>
2024-12-07 16:59:24 +0100

    feat: bonus function

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 19:47:53 by rha-le            #+#    #+#             */
-/*   Updated: 2024/12/06 19:52:11 by rha-le           ###   ########.fr       */
+/*   Updated: 2024/12/07 15:24:20 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
  *
  * @param lst The address of a pointer to the first link of a list.
  * @param new The address of a pointer to the node to be added to the list.
+ * @return None
  */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
@@ -25,23 +26,23 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		ft_lstlast(*lst)->next = new;
 }
 
-#include <stdio.h>
-int main(void)
-{
-	t_list	*names_empty = NULL;
-
-	ft_lstadd_back(&names_empty, ft_lstnew("Robbie"));
-	printf("test on empty: %s\n", (char *)ft_lstlast(names_empty)->content);
-
-	t_list *names = NULL;
-	ft_lstadd_front(&names, ft_lstnew("A2"));
-	ft_lstadd_front(&names, ft_lstnew("9S"));
-	ft_lstadd_front(&names, ft_lstnew("2B"));
-	ft_lstadd_back(&names, ft_lstnew("Robbie"));
-	t_list *temp = names;
-	while (temp)
-	{
-		printf("%s\n", (char *)temp->content);
-		temp = temp->next;
-	}
-}
+/*#include <stdio.h>*/
+/*int main(void)*/
+/*{*/
+/*	t_list	*names_empty = NULL;*/
+/**/
+/*	ft_lstadd_back(&names_empty, ft_lstnew("Robbie"));*/
+/*	printf("test on empty: %s\n", (char *)ft_lstlast(names_empty)->content);*/
+/**/
+/*	t_list *names = NULL;*/
+/*	ft_lstadd_front(&names, ft_lstnew("A2"));*/
+/*	ft_lstadd_front(&names, ft_lstnew("9S"));*/
+/*	ft_lstadd_front(&names, ft_lstnew("2B"));*/
+/*	ft_lstadd_back(&names, ft_lstnew("Robbie"));*/
+/*	t_list *temp = names;*/
+/*	while (temp)*/
+/*	{*/
+/*		printf("%s\n", (char *)temp->content);*/
+/*		temp = temp->next;*/
+/*	}*/
+/*}*/
---
+++ libft/ft_lstmap.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 15:27:54 by rha-le            #+#    #+#             */
+/*   Updated: 2024/12/07 16:58:51 by rha-le           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+/**
+ * @brief Creates a new list resulting from applying a function to each element.
+ *
+ * Iterates through the list ’lst’ and applies the function ’f’
+ * to the content of each node. A new list is created containing
+ * the results of the successive applications of the function ’f’.
+ * The ’del’ function is used to delete the content of a node if
+ * allocation or other operations fail.
+ *
+ * @param lst A pointer to the first node of the list.
+ * @param f The function to apply to each node's content.
+ * @param del The function to delete a node's content, if needed.
+ * @return A pointer to the new list, or NULL if memory allocation fails.
+ */
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*res;
+	t_list	*new_node;
+
+	res = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, new_node);
+		lst = lst->next;
+	}
+	return (res);
+}
+
+/*static void	*rot1(void *content)*/
+/*{*/
+/*	char	*src;*/
+/*	char	*new_str;*/
+/*	int		i;*/
+/**/
+/*	src = (char *)content;*/
+/*	new_str = ft_strdup(src);*/
+/*	if(!new_str)*/
+/*		return (NULL);*/
+/*	i = 0;*/
+/*	while (new_str[i])*/
+/*	{*/
+/*		new_str[i] += 1;*/
+/*		i++;*/
+/*	}*/
+/*	return (new_str);*/
+/*}*/
+
+/*#include <stdio.h>*/
+/*int main(void)*/
+/*{*/
+/*	t_list *alpha = NULL;*/
+/*	ft_lstadd_back(&alpha, ft_lstnew("abc"));*/
+/*	ft_lstadd_back(&alpha, ft_lstnew("def"));*/
+/*	ft_lstadd_back(&alpha, ft_lstnew("ghi"));*/
+/*	ft_lstadd_back(&alpha, ft_lstnew("jkl"));*/
+/**/
+/*	t_list *temp = alpha;*/
+/*	printf("Source List:\n");*/
+/*	while (temp)*/
+/*	{*/
+/*		printf("%s\n", (char *)temp->content);*/
+/*		temp = temp->next;*/
+/*	}*/
+/**/
+/*	t_list *alpha_rot1;*/
+/*	alpha_rot1 = ft_lstmap(alpha, rot1, free);*/
+/*	temp = alpha_rot1;*/
+/*	printf("\nMapped List:\n");*/
+/*	while (temp)*/
+/*	{*/
+/*		printf("%s\n", (char *)temp->content);*/
+/*		temp = temp->next;*/
+/*	}*/
+/*}*/
