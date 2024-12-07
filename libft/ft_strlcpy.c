b5b89f34f015f6668362dd3152318658ad165751
Robbie Nhat Anh Ha Le <rha-le@c3a10c6.42berlin.de>
2024-12-07 18:26:35 +0100

    fix: removed unnecassry header

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 size_t	ft_strlcpy(char dest[], const char *src, size_t size)
 {
