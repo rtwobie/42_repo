93c53a292fda558b6534e0e7361dfe44c82e8687
Robbie Nhat Anh Ha Le <rha-le@c3a10c6.42berlin.de>
2024-12-07 18:19:12 +0100

    fix: removed <string.h>

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
